ec234a6a73de303cd81a2683dbc9596094dfda16
Seaborn Lee <seabornlee.cn@gmail.com>
2020-12-06 18:45:31 +0800

    style: format

--- src/GildedRoseTextTests.c
+++ src/GildedRoseTextTests.c
@@ -1,19 +1,19 @@
+// Copyright 2020 Seaborn Lee
+
 #include <stdio.h>
 #include "GildedRose.h"
 
 int
-print_item(Item *item)
-{
+print_item(Item *item) {
     return printf("%s, %d, %d\n", item->name, item->sellIn, item->quality);
 }
 
-int main()
-{
+int main() {
     Item items[9];
     int last = 0;
     int day;
     int index;
-    
+
     init_item(items + last++, "+5 Dexterity Vest", 10, 20);
     init_item(items + last++, "Aged Brie", 2, 0);
     init_item(items + last++, "Elixir of the Mongoose", 5, 7);
@@ -24,17 +24,16 @@ int main()
     init_item(items + last++, "Backstage passes to a TAFKAL80ETC concert", 5, 49);
     // this Conjured item doesn't yet work properly
     init_item(items + last++, "Conjured Mana Cake", 3, 6);
-    
+
     puts("OMGHAI!");
 
-    for (day = 0; day <= 30; day++)
-    {
+    for (day = 0; day <= 30; day++) {
         printf("-------- day %d --------\n", day);
         printf("name, sellIn, quality\n");
-        for(index = 0; index < last; index++) {
+        for (index = 0; index < last; index++) {
             print_item(items + index);
         }
-	
+
         printf("\n");
 
         update_quality(items, last);
--- test/GildedRoseUnitTests.cc
+++ test/GildedRoseUnitTests.cc
@@ -1,3 +1,5 @@
+// Copyright 2020 Seaborn Lee
+
 #include <gtest/gtest.h>
 #include <ApprovalTests.hpp>
 
