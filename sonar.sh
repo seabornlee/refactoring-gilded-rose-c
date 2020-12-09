4b00a580708f8d0e6338acc33e907e9b43582699
Seaborn Lee <seabornlee.cn@gmail.com>
2020-12-10 02:29:18 +0800

    remove docker command

--- sonar.sh
+++ sonar.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
 sed "s/user/$1/g" sonar-project.properties > src/sonar-project.properties
-
-docker run \
-    --rm \
-    -e SONAR_HOST_URL="$2" \
-    -v "${PWD}/src/:/usr/src" \
-    sonarsource/sonar-scanner-cli
